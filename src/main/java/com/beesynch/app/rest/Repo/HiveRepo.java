b81346e6893e9b800c06fc9e865da8c2e9b579fc
Jascent Pearl G. Navarro <111488181+jassynavarro@users.noreply.github.com>
2025-02-26 02:23:39 +0800

    Delete src directory

--- src/main/java/com/beesynch/app/rest/Repo/HiveRepo.java
+++
@@ -1,6 +0,0 @@
-package com.beesynch.app.rest.Repo;
-import  com.beesynch.app.rest.Models.Hive;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface HiveRepo extends JpaRepository<Hive, Long>{
-    boolean existsByHiveName(String hiveName);
-}
